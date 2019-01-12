14696dff9aa006817bacd651296b64bff027ff4d
Or Mauda <ormauda@gmail.com>
2019-01-12 13:30:50 +0200

    embedded youtube video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'shamu';
+    title = 'shamu';
+
+    safeURL: SafeResourceUrl;
+    
+    constructor(private _sanitizer: DomSanitizer) {
+        const videoURL = 'https://www.youtube.com/embed/Tp4a3I2Ikko';
+        this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(videoURL);
+    }
 }
